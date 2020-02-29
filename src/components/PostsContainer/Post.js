eb7a92843f4d77bc9f3ac3cea473e949e561bc1d
AmandaCallender <amanda.callender@outlook.com>
2020-02-28 20:14:03 -0500

    Second Commit, Worked on Posts.js

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,22 +1,20 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
-
+import dummyData from "../../dummy-data.js";
 import "./Posts.css";
 
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
-
+  const [likes, updateLikes] = useState(props.post.likes);
   return (
     <div className="post-border">
       <PostHeader
         username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
+        thumbnailUrl={props.post.thumbnailUrl}
       />
       <div className="post-image-wrapper">
         <img
@@ -25,7 +23,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection likes={likes} updateLikes={updateLikes} />
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
@@ -35,5 +33,3 @@ const Post = props => {
 };
 
 export default Post;
-
-
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,19 +1,21 @@
 //Complete the necessary code in this file
 // import useState
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
-
-const PostsPage = () => {
+// import data
+import dummyData from "../../dummy-data.js";
+import LikeSection from "./LikeSection";
+const PostsPage = (props) => {
   // set up state for your data
-  const [color, setColor]
+  const [data, updateData] = useState(dummyData)
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {/data.map((elem,i) => {
+        return <Post post = {elem} key = {i} />
+      })}
     </div>
   );
 };
 
 export default PostsPage;
-
